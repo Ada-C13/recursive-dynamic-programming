f68a097bd63bd0f92cc17cfc59e1dcda1f62005a
Jessica Liang <jessicursliang@gmail.com>
2020-04-30 18:37:40 -0700

    formatting changes

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -2,19 +2,18 @@
 
 # Time Complexity - O(n), with n being the value of the integer.
 # Space Complexity - O(n), because of the recursive stack.
-# Hint, you may want a recursive helper method
 def fibonacci(n)
-	raise ArgumentError if n < 0
-	return 0 if n == 0
-	return 1 if n == 1 || n == 2
+  raise ArgumentError if n < 0
+  return 0 if n == 0
+  return 1 if n == 1 || n == 2
 
-	return fib_helper(n, 2, 1, 1, 1)
+  return fib_helper(n, 2, 1, 1, 1)
 end
 
 def fib_helper(target, current, one, two, sum)
-	if current < target
-		return fib_helper(target, current + 1, one = two, two = sum, two + one)
-	end
+  if current < target
+    return fib_helper(target, current + 1, one = two, two = sum, two + one)
+  end
 
-	return sum
+  return sum
 end
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,31 +3,31 @@
 # Time Complexity - O(n), with n being the number of digits in the initial integer.
 # Space Complexity - O(n), because of the recursive stack; size of the stack depends on the number of digits.
 def super_digit(n)
-	sum = 0
+  sum = 0
 
-	if n < 10
-		return sum + n
-	else
-		until n < 10
-			sum += (n % 10)
-			n /= 10
-		end
+  if n < 10
+    return sum + n
+  else
+    until n < 10
+      sum += (n % 10)
+      n /= 10
+    end
 
-		sum += n
-	end
+    sum += n
+  end
 
-	return super_digit(n = sum)
+  return super_digit(n = sum)
 end
 
 # Time Complexity - O(n * k), with n being the number of digits in the n initially, and k being the number of times more
 # Space Complexity - O(n), because of the recursive stack.
 def refined_super_digit(n, k)
-	sum = 0
+  sum = 0
 
-	k.times do
-		sum += super_digit(n)
-	end
+  k.times do
+    sum += super_digit(n)
+  end
 
-	return super_digit(n = sum)
+  return super_digit(n = sum)
 end
     
