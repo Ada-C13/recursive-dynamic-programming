bc6ffd6a36ae867ef4fc0d711eba25898a466c47
Faezeh Ashtiani <faezeh.ashtiani@gmail.com>
2020-05-02 15:15:58 -0700

    passes all tests

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,8 +1,20 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Time Complexity - O(n) 
+# Space Complexity - ?  (should be O(n)) - yes it is , because of the call stack!
 # Hint, you may want a recursive helper method
-def fibonacci(n)
- 
+def fibonacci(num)
+  raise ArgumentError if num.negative?
+
+  fibonacci_helper([0, 1], 2, num)
+end
+
+def fibonacci_helper(array, current, fib_num)
+  return fib_num if fib_num.zero? || fib_num == 1
+
+  return array[0] + array[1] if current == fib_num
+
+  array[0], array[1] = array[1], array[0] + array[1]
+
+  fibonacci_helper(array, current + 1, fib_num)
 end
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,30 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
-def super_digit(n)
-   
-end
-  
+# me and Jessica worked through these problems together :)
 
-# Time Complexity - ?
-# Space Complexity - ?
-def refined_super_digit(n, k)
-    
+# Time Complexity - each time super helper runs it is O(num.length)
+# => O(n) but really it is O(log n of the num in the base of 10)
+# and it might run multiple times until number becomes less than 10 but that's a
+# constant and we drop it.
+# Space Complexity - O(n) based on the above logic
+def super_digit(number)
+  number = super_helper(number) until number < 10
+  number
 end
-    
+
+def super_helper(num)
+  if num < 10
+    num
+  else
+    (num % 10) + super_helper(num / 10)
+  end
+end
+
+# Time Complexity - similar to the above method except that one time
+# we create a new string and new number but since it is constatnt no matter what
+# parameters are, it does not increase the time or space complexity.
+# Space Complexity - ^
+def refined_super_digit(num, times)
+  new_num = (num.to_s * times).to_i
+  super_digit(new_num)
+end
