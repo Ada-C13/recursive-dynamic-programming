b6c817824714d66243a109eae7b3e2c935e058e0
Rosalyn <syrosalynyu@gmail.com>
2020-05-02 17:40:00 -0700

    completed fibonacci Space Complexity O(1) and passed tests

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,8 +1,22 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Time Complexity - ? O(n). 
+# Space Complexity - ?  O(1). I didn't create an array. Instead, I create constants to hold the second last and third last value
 # Hint, you may want a recursive helper method
 def fibonacci(n)
- 
+  raise ArgumentError.new("Can'd take nagative number. INPUT: #{n} ") if n < 0
+  fib_helper(1, 0, 2, n)
+end
+
+def fib_helper(second_last, third_last, current_index, n)
+  return n if n == 0 || n == 1
+
+  if current_index == n
+    return second_last + third_last
+  end
+
+  last = second_last + third_last
+  third_last = second_last
+  second_last = last
+  return fib_helper(second_last, third_last, current_index + 1, n)
 end
