db6f1dacf77a24c23cf5a3cb9eb9ea3273095ad1
Catherina <catherine.pchelina@gmail.com>
2020-05-03 12:35:28 -0700

    Implement improved fibonacci

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,8 +1,26 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Time Complexity - O(n)
+# Space Complexity - O(n)  
 # Hint, you may want a recursive helper method
-def fibonacci(n)
- 
+
+def fib_helper(last, second_to_last, current, n)
+  return n if n == 0 || n == 1
+
+  if current == n
+    return last + second_to_last
+  end
+
+  sum = last + second_to_last
+  last = second_to_last
+  second_to_last = sum
+
+  return fib_helper(last, second_to_last, current + 1, n)
 end
+
+def fibonacci(n)
+  raise ArgumentError if n < 0
+  fib_helper(0, 1, 2, n)
+end
+
+
