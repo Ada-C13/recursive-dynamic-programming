f4420585055f48f2aca92b62299865955ba6372c
vwhwang <vwhwang38@gmail.com>
2020-05-03 15:41:27 -0700

    completed superdigit but can't think of fibi

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -3,6 +3,25 @@
 # Time Complexity - ?
 # Space Complexity - ?  (should be O(n))
 # Hint, you may want a recursive helper method
+
+# couldn't think of a new way 
 def fibonacci(n)
- 
+  if n < 0 
+    raise ArgumentError.new "n should be greather or equal to 0"
+  end 
+  return fibonacci_helper([0,1],2,n)
 end
+
+
+def fibonacci_helper(solution, current, n)
+  return n if n == 0 || n == 1 
+
+  if current == n 
+    return solution[n-1] + solution[n-2]
+  else
+    solution << solution[current-1] + solution[current-2]
+  end 
+
+  return fibonacci_helper(solution,current+1, n)
+  
+end 
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,37 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n)
+# Space Complexity - O(n)
 def super_digit(n)
-   
+  if n < 10 
+    return n 
+  end 
+
+  array = n.to_s.split("")
+  answer = array.map{|n|n.to_i}.sum
+  return super_digit(answer)
+    
 end
   
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n)
+# Space Complexity - O(n)
 def refined_super_digit(n, k)
-    
+
+  array = k.to_s.split("")
+  first = array.map{|k|k.to_i}.sum * n 
+
+  if first < 10 
+    return first 
+  end 
+
+  new_num = first 
+  while new_num > 9
+    new_array = new_num.to_s.split("")
+    new_num = new_array.map{|n|n.to_i}.sum
+  end 
+
+  return new_num
+
 end
     
