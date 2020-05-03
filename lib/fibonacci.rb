8cd4c50104a2d2847197b17ee294e43f3432c390
vwhwang <vwhwang38@gmail.com>
2020-05-03 16:23:46 -0700

    completed

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,27 +1,48 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
+# Time Complexity - O(1)
 # Space Complexity - ?  (should be O(n))
 # Hint, you may want a recursive helper method
 
-# couldn't think of a new way 
+
 def fibonacci(n)
   if n < 0 
     raise ArgumentError.new "n should be greather or equal to 0"
   end 
-  return fibonacci_helper([0,1],2,n)
+  return fibonacci_helper(0,1,2,n)
 end
 
 
-def fibonacci_helper(solution, current, n)
-  return n if n == 0 || n == 1 
+def fibonacci_helper(last_1, last_2, current , n )
+  return n if n == 0|| n == 1 
 
-  if current == n 
-    return solution[n-1] + solution[n-2]
+  if current == n
+    return last_1 + last_2
   else
-    solution << solution[current-1] + solution[current-2]
+    return fibonacci_helper(last_2,last_1 + last_2 ,current + 1 , n)
   end 
-
-  return fibonacci_helper(solution,current+1, n)
-  
 end 
+
+
+
+
+# def fibonacci(n)
+#   if n < 0 
+#     raise ArgumentError.new "n should be greather or equal to 0"
+#   end 
+#   return fibonacci_helper([0,1],2,n)
+# end
+
+
+# def fibonacci_helper(solution, current, n)
+#   return n if n == 0 || n == 1 
+
+#   if current == n 
+#     return solution[n-1] + solution[n-2]
+#   else
+#     solution << solution[current-1] + solution[current-2]
+#   end 
+
+#   return fibonacci_helper(solution,current+1, n)
+  
+# end 
