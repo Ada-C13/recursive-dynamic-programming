b193f242de00439d3a2bad01ae8aee167011f410
quinqu <quinruby0@gmail.com>
2020-05-03 11:29:40 -0700

    fixed super digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -2,10 +2,33 @@
 
 # Time Complexity - ?
 # Space Complexity - ?
-def super_digit(n)
-   
-end
+
+#get the sum 
+#pass back in 
+
+
+
+def super_digit(num) #9875
+  return super_digit_helper(0, num)
+end 
+
+def super_digit_helper(sum, num)
+   if num < 10
+      return num 
+   end 
+
+   while num % 10 != num  
+    sum += num%10
+    num = num/10
+   end 
+
+   if num%10 == num 
+    sum += num
+   end 
   
+   return super_digit_helper(0, sum)
+end 
+
 
 # Time Complexity - ?
 # Space Complexity - ?
@@ -13,3 +36,4 @@ def refined_super_digit(n, k)
     
 end
     
+puts super_digit(9875)
