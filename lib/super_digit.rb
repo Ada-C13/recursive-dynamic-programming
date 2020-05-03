e214a132027c797422581fa67fc87c67390d59ab
quinqu <quinruby0@gmail.com>
2020-05-03 12:03:41 -0700

    optional super digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,12 +3,7 @@
 # Time Complexity - ?
 # Space Complexity - ?
 
-#get the sum 
-#pass back in 
-
-
-
-def super_digit(num) #9875
+def super_digit(num)
   return super_digit_helper(0, num)
 end 
 
@@ -32,8 +27,31 @@ end
 
 # Time Complexity - ?
 # Space Complexity - ?
+# n.length = 4 -> 10000
+# n.length > 3 -> * 1000
+# n.length = 2 -> * 100 
+# n.length = 1 -> * 10 
+
 def refined_super_digit(n, k)
-    
+
+  num = 0
+  while k > 0 
+
+    if k == 3
+      num += (n*1000000)
+    elsif k == 2
+      num += (n*1000)
+
+    elsif k == 1
+      num += n
+
+    elsif k == 4
+      num += (n*1000000000)
+    end 
+
+    k -= 1
+  end 
+  return super_digit_helper(0, num)
 end
     
-puts super_digit(9875)
+puts refined_super_digit(148, 3)
--- test/super_digit_test.rb
+++ test/super_digit_test.rb
@@ -33,29 +33,29 @@ describe "super_digit" do
     expect(answer).must_equal 6
   end
 
-  # describe "refined superdigit" do 
-  #   it "will return 1 for n = 1 and k = 1" do
-  #     # Act
-  #     answer = refined_super_digit(1, 1)
+  describe "refined superdigit" do 
+    it "will return 1 for n = 1 and k = 1" do
+      # Act
+      answer = refined_super_digit(1, 1)
 
-  #     # Assert
-  #     expect(answer).must_equal 1      
-  #   end
+      # Assert
+      expect(answer).must_equal 1      
+    end
 
-  #   it "will return 8 for n=9875 and k = 4" do
-  #     # Act
-  #     answer = refined_super_digit(9875, 4)
+    it "will return 8 for n=9875 and k = 4" do
+      # Act
+      answer = refined_super_digit(9875, 4)
 
-  #     # Assert
-  #     expect(answer).must_equal 8
-  #   end
+      # Assert
+      expect(answer).must_equal 8
+    end
 
-  #   it "will return 3 for n=148 and k = 3" do
-  #       # Act
-  #       answer = refined_super_digit(148, 3)
+    it "will return 3 for n=148 and k = 3" do
+        # Act
+        answer = refined_super_digit(148, 3)
   
-  #       # Assert
-  #       expect(answer).must_equal 3
-  #   end
-  # end
+        # Assert
+        expect(answer).must_equal 3
+    end
+  end
 end
