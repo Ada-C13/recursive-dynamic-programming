90044349091db77a3a872d9d872a86032f7a3efa
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-04 12:22:50 -0700

    adds pseudocode for a recursive dynamic solution with a helper

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -4,5 +4,17 @@
 # Space Complexity - ?  (should be O(n))
 # Hint, you may want a recursive helper method
 def fibonacci(n)
-  return n <= 1 ? n : fibonacci(n-1) + fibonacci(n-2)
+  raise ArgumentError.new("Error: Number is less than zero!") if n < 0
+  return fib_helper([0, 1], 2, n) # we want to start current at 2 because it is the first number that won't trigger line 12
+end
+
+def fib_helper(solutions, current, n)
+  return n if n <= 1
+
+  # if n reaches the current number
+  # return the sum of values at n-1 and n-2 in the solutions array
+
+  # otherwise push the sum of values current-1 and current-2 into the solutions array
+
+  # return fibonacci_helper with current incremented by 1
 end
