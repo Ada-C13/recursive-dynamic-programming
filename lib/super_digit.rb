eb9a0ff39e09509c7ec5a2e159eaa339fb743d45
Alicia Combs <alicia.combs.92@gmail.com>
2020-05-04 16:39:18 -0700

    Successfully pass all tests for super_digit method

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,7 +3,15 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-   
+  #use power of 10 shit to calculate each place
+  #if it's an integer, it'll just chop off what's after the decimal point!
+
+  #OR reduce to a string, etc
+  nums = n.to_s.split('')
+  
+  total = (nums.map { |str| str.to_i }).sum
+  return total if total < 10
+  return super_digit(total)
 end
   
 
