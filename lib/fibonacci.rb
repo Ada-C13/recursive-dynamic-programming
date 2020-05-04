ca9e02e8b3306ae56f6afb7102afd8b84665aafd
Alicia Combs <alicia.combs.92@gmail.com>
2020-05-04 16:23:41 -0700

    Fibonacci method passes all tests

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,8 +1,18 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Time Complexity - O(n)
+# Space Complexity - O(n)  (should be O(n))
 # Hint, you may want a recursive helper method
+FIB_NUMS = { 0 => 0, 1 => 1}
+
 def fibonacci(n)
- 
+  raise ArgumentError if n < 0
+  if n <= 1 
+    return n
+  elsif FIB_NUMS[n]
+    return FIB_NUMS[n]
+  else
+    FIB_NUMS[n] = fibonacci(n - 1) + fibonacci(n - 2)
+    return FIB_NUMS[n]
+  end
 end
