b9df638567b68b156384f2a9b9bb08d113ba5c66
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-04 13:46:35 -0700

    defines more efficient solution for super_digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,18 +1,17 @@
 # Superdigit
 
 # Time Complexity - O(n)
-# Space Complexity - O(n^2)
+# Space Complexity - O(n)
 def super_digit(n)
   return n if n < 10 
   
-  # we need to get space complexity down
-  # what does digits actually do?
-  # it calculates n % 10, then n / 10
-  # following that logic, maybe we can create a sum variable that will store the sum of the digits of each number, which will then be fed back into the recursive method
-  # so we need to add n % 10 to the sum, then divide n by 10 each time
-  # and this would repeat until n is 0
+  sum = 0
+  until n == 0
+    sum += n % 10
+    n /= 10
+  end
   
-  return super_digit(n.digits.sum)
+  return super_digit(sum)
 end
 
 # Time Complexity - ?
