4c39d464cf48c831c1531919b92483835ef12fec
Yieni <yaynee8d@gmail.com>
2020-05-03 22:00:38 -0700

    refactor refined super digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -18,23 +18,28 @@ end
 
 # Time Complexity - O(k+n) where n is the number of digits in n?
 # Space Complexity - O(n) because I am using recursion ?
-def refined_helper(n, k)
+# def refined_helper(n, k)
+#   string_n = n.to_s
+#   repeat_n = ""
+#   k.times do
+#     repeat_n.concat(string_n)
+#   end
+#   return repeat_n
+# end
+
+def refined_super_digit(n, k)
   string_n = n.to_s
+  return n if string_n.size == 1
+
   repeat_n = ""
   k.times do
     repeat_n.concat(string_n)
   end
-  return repeat_n
-end
-
-def refined_super_digit(n, k)
-  string_n = refined_helper(n, k)
-  return n if string_n.size == 1
 
   i = 1
-  sum = string_n[0].to_i
-  while i < string_n.size
-    sum += string_n[i].to_i
+  sum = repeat_n[0].to_i
+  while i < repeat_n.size
+    sum += repeat_n[i].to_i
     i += 1
   end
   return super_digit(sum)
