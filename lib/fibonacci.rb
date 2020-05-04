d188d50f6d69528becef5b491717e0cbbd426ec4
Alex Robertson <arobertson335@gmail.com>
2020-05-04 16:38:08 -0700

    Submitting assingment.

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,8 +1,25 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
-# Hint, you may want a recursive helper method
-def fibonacci(n)
- 
-end
+# Time Complexity - O(n)
+# Space Complexity - O(n)
+
+def improved_fibonacci(n)
+  raise ArgumentError.new("Cannot return the fibonacci of a negative number.") if n < 0
+  helper([0,1], n) 
+end 
+
+def helper(sequence, n)
+  relevant_pair_sum = sequence[0] + sequence[1]
+  sequence[0] = sequence[1]
+  sequence[1] = relevant_pair_sum
+
+  return n if n == 0 || n == 1
+  return relevant_pair_sum if n == 2 
+  return helper(sequence, n - 1)
+end 
+
+# Test cases
+puts improved_fibonacci(5)
+puts improved_fibonacci(7)
+puts improved_fibonacci(11)
+puts improved_fibonacci(-1)
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,18 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
-def super_digit(n)
-   
-end
-  
+# Time Complexity - O(n)
+# Space Complexity - O(n) 
+# Proc source: https://www.alexstrick.com/blog/using-rubys-digits-method
 
-# Time Complexity - ?
-# Space Complexity - ?
-def refined_super_digit(n, k)
-    
+def super_digit(n)
+  return n if n <= 9
+
+  digits = n.to_s.chars.map { |digit| digit.to_i }
+  sum = digits.sum
+
+  return super_digit(sum)
 end
-    
+
+# Test cases
+puts super_digit(9875)
+puts super_digit(5)
