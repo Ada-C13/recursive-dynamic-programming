6803dc551a6e813556c372f1dfb2cad41966298b
cojenco <icathyouyang@gmail.com>
2020-05-03 18:21:56 -0700

    revise refined_super_digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -18,37 +18,39 @@ end
 # Refined Superdigit
 # Time Complexity - O(log10n)
 # Space Complexity - O(log10n)
-def refined_super_digit(n, k)
-  raise ArgumentErrof if n < 0
-  return n if n < 10      # base case
-
-  sum = 0
-  while n / 10 > 0
-    sum += n % 10
-    n /= 10
-  end
-  sum += n
-
-  if k > 0
-    sum *= k
-    k = 0
-  end
-  
-  return refined_super_digit(sum, k)
+def refined_super_digit(n,k)
+  return super_digit(n*k)
 end
 
 
-# Using 2 recursions 
+
+# Refined Superdigit
+# def refined_super_digit(n, k)
+#   raise ArgumentErrof if n < 0
+#   return n if n < 10      # base case
+#   sum = 0
+#   while n / 10 > 0
+#     sum += n % 10
+#     n /= 10
+#   end
+#   sum += n
+#   if k > 0
+#     sum *= k
+#     k = 0
+#   end
+#   return refined_super_digit(sum, k)
+# end
+
+
+# Superdigit: Using 2 recursions 
 # def super_digit(n)
 #   raise ArgumentErrof if n < 0
 #   # base case: when n < 10
 #   return n if n < 10
-
 #   # recursive case
 #   sum = sum_digits(n)
 #   return super_digit(sum)
 # end
-  
 # def sum_digits(n)
 #   if n < 10 
 #     return n
