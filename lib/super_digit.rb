1ced10016e11b5826523e7fe5f242c541b3f9aab
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-04 12:52:52 -0700

    adds pseudocode for super_digit method

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,7 +3,10 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-   
+  return n if n < 10 # if the number has only one digit, return that number. this could be the base case of a recursive method
+  
+  # split the number up into its digits
+  # add each digit together and feed it back into the super_digit method
 end
   
 
