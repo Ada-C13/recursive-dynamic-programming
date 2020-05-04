ccccfae957bce296256e978cf8c531dd28d1e684
Antonia <antonianomamiukor@gmail.com>
2020-05-04 16:22:39 -0700

    completed

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,16 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n)
+# Space Complexity - O(n)
 def super_digit(n)
-   
+  return n if n < 10
+  nums = n.digits
+  super_digit(nums.sum)
 end
-  
+
 
 # Time Complexity - ?
 # Space Complexity - ?
-def refined_super_digit(n, k)
-    
-end
-    
+# def refined_super_digit(n, k)
+
+# end
