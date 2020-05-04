f63a49bec53a20e5d01f7197da9c9f51e91a47dc
Corinna Fabre <corinna.fabre@gmail.com>
2020-05-03 20:10:34 -0700

    super digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,16 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n)
+# Space Complexity - O(n)
 def super_digit(n)
-   
+  return n if n.digits.length == 1
+
+  super_digit(n.digits.sum)
 end
-  
 
 # Time Complexity - ?
 # Space Complexity - ?
 def refined_super_digit(n, k)
-    
+  return (super_digit(n) * k)
 end
     
