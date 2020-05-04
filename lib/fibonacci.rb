b748977974db318dd243de2cc192f6e193695278
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-05-03 22:16:26 -0700

    recursive dynamic

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,8 +1,20 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Time Complexity - O(n)
+# Space Complexity - O(n)
 # Hint, you may want a recursive helper method
 def fibonacci(n)
- 
+  raise ArgumentError if n < 0
+  return fib_helper([0, 1], 2, n)
+end
+
+def fib_helper(solutions, current, n)
+  return n if n == 0 || n == 1
+  
+  if current == n
+    return solutions[0] + solutions[1]
+  end
+  
+  solutions = [solutions[-1], solutions[0] + solutions[1]]
+  return fib_helper(solutions, current + 1, n)
 end
