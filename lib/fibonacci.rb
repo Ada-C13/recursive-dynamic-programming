1b4e476183bdce9c32d80d8a296666859db85027
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-04 12:38:47 -0700

    defines a recursive dynamic solution for fibonacci, passes all tests

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,20 +1,19 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
-# Hint, you may want a recursive helper method
+# Time Complexity - O(n)
+# Space Complexity - O(n), but could we get this to O(1) using a hash table somehow?
 def fibonacci(n)
   raise ArgumentError.new("Error: Number is less than zero!") if n < 0
-  return fib_helper([0, 1], 2, n) # we want to start current at 2 because it is the first number that won't trigger line 12
+  
+  return fib_helper([0, 1], 2, n)
 end
 
 def fib_helper(solutions, current, n)
   return n if n <= 1
+  
+  return solutions[n - 1] + solutions[n - 2] if current == n
 
-  # if n reaches the current number
-  # return the sum of values at n-1 and n-2 in the solutions array
+  solutions << solutions[current - 1] + solutions[current - 2]
 
-  # otherwise push the sum of values current-1 and current-2 into the solutions array
-
-  # return fibonacci_helper with current incremented by 1
+  return fib_helper(solutions, current + 1, n)
 end
