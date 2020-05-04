0369b31b0b21302a88ef7d9c101f1a021d5d8ed4
Yieni <yaynee8d@gmail.com>
2020-05-03 21:29:26 -0700

    complete questions

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,8 +1,22 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Time Complexity - O(n)?
+# Space Complexity - O(n)?  (should be O(n))
 # Hint, you may want a recursive helper method
+
+def fib_helper(n, i, hash)
+  raise ArgumentError if n < 0
+  return hash[n-1] + hash[n-2] if i == n
+
+  if i <= n
+    hash[i]= hash[i-1] + hash[i-2]
+    i += 1
+  end
+  return fib_helper(n, i, hash)
+end
+  
 def fibonacci(n)
- 
+  raise ArgumentError if n < 0
+  return n if n <= 1
+  fib_helper(n, 2, {0 => 0, 1 => 1})
 end
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,42 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity O(n) because I am using a loop ?
+# Space Complexity 0(n) becuase I am using recursion?
 def super_digit(n)
-   
+  string_n = n.to_s
+  return n if string_n.size == 1
+
+  i = 1
+  sum = string_n[0].to_i
+  while i < string_n.size
+    sum += string_n[i].to_i
+    i += 1
+  end
+  return super_digit(sum)
 end
   
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(k+n) where n is the number of digits in n?
+# Space Complexity - O(n) because I am using recursion ?
+def refined_helper(n, k)
+  string_n = n.to_s
+  repeat_n = ""
+  k.times do
+    repeat_n.concat(string_n)
+  end
+  return repeat_n
+end
+
 def refined_super_digit(n, k)
-    
+  string_n = refined_helper(n, k)
+  return n if string_n.size == 1
+
+  i = 1
+  sum = string_n[0].to_i
+  while i < string_n.size
+    sum += string_n[i].to_i
+    i += 1
+  end
+  return super_digit(sum)
 end
     
