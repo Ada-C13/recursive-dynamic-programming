66d39c05af498cbed38d9a3628880fb000b6a57b
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-04 15:29:11 -0700

    defines refined_super_digit method using super_digit as a helper

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,7 +1,7 @@
 # Superdigit
 
-# Time Complexity - O(n)
-# Space Complexity - O(n)
+# Time Complexity - O(log10n)
+# Space Complexity - O(log10n)
 def super_digit(n)
   return n if n < 10 
   
@@ -11,12 +11,19 @@ def super_digit(n)
     n /= 10
   end
   
-  return super_digit(sum)
+  return super_digit(sum) # I had n.digits.sum here earlier, but since #digits creates a new array, I thought this solution would be less expensive
 end
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - Still O(log10n)?
+# Space Complexity - Still O(log10n)?
 def refined_super_digit(n, k)
-    
+  sum = 0
+  until n == 0
+    sum += n % 10
+    n /= 10
+  end
+
+  n = sum * k
+  return super_digit(n)
 end
     
