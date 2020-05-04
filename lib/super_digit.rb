ed1f28fc07845cb9f26a956b1c1e6621a970bf3c
Alicia Combs <alicia.combs.92@gmail.com>
2020-05-04 16:42:53 -0700

    Pass all tests for refined_super_digit method

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,12 +1,8 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n2)
+# Space Complexity - O(2n)
 def super_digit(n)
-  #use power of 10 shit to calculate each place
-  #if it's an integer, it'll just chop off what's after the decimal point!
-
-  #OR reduce to a string, etc
   nums = n.to_s.split('')
   
   total = (nums.map { |str| str.to_i }).sum
@@ -18,6 +14,10 @@ end
 # Time Complexity - ?
 # Space Complexity - ?
 def refined_super_digit(n, k)
-    
+  nums = n.to_s.split('') * k
+  
+  total = (nums.map { |str| str.to_i }).sum
+  return total if total < 10
+  return super_digit(total)
 end
     
