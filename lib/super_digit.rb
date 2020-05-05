d83c2d53201a0e726ee79222582c949c8f6c0eb8
Haben <haben7@gmail.com>
2020-05-04 19:13:19 -0700

    super_digit completed

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,33 +1,20 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - ? O(n)
+# Space Complexity - ? O(n)
 
 #Given an integer, we need to find the super digit of the integer.
-
-# super_digit(9875) --> superdigit(9 + 8 + 7 + 5) = superdigit(29)
-# super_digit(29)   --> superdigit(2 + 9) = superdigit(11)
-# super_digit(11)   --> superdigit(1 + 1) = superdigit(2)
-# super_digit(2) --> 2
 
 def super_digit(n)
   return n if n < 10
 
-  array = n.to_s.chars # ('9'  '8' '7'  '5')
-  new_array = array.map(&:to_i) # [9 ,8, 7, 5]
-  something = new_array.sum # 29
+  chars_array = n.to_s.chars # ('9'  '8' '7'  '5')
+  int_array = chars_array.map(&:to_i) # [9 ,8, 7, 5]
+  sum = int_array.sum # 29
 
-  super_digit(something)
+  super_digit(sum)
 end
-# return n if n < 10
-#   return super_digit(n.to_s.chars.map(&:to_i).sum)
 
-#  In this exercise we will build on the Superdigit concept. 
-#  A refined superdigit is a determined by concatenating 
-#  a numbrer `n` a specific `k` number of times and then 
-#  calculating the superdigit.  
-
-#For example if k = 3 and n = 148
 
 # Time Complexity - O(n)
 # Space Complexity - O(1)
