1718cb73ed22bdf7d87f3bf42eedfc4c0ff1d0c6
Vera <lizeth.veraro@outlook.com>
2020-05-04 18:52:14 -0700

    Create Fibonacci class and change the test to accept the class

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,8 +1,39 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Time Complexity - O(n)
+# Space Complexity - 0(n)
+# 0(n * 3) Since I have 3 variables, but if we get rid of the constant. 
+# Will be O(n)
 # Hint, you may want a recursive helper method
 def fibonacci(n)
  
+end
+
+class Fibonacci
+  def self.fibonacci(n)
+    @number1 = 0
+    @number2 = 1
+    @number3 = 0
+    return fib_helper(n)
+  end
+
+  private
+
+  def self.fib_helper(n)
+    if n == 0 || n == 1
+      return n
+    elsif n < 0
+      raise ArgumentError
+    end
+
+    if n > 2
+      fib_helper(n - 1)
+    end
+
+    @number3 = @number1 + @number2
+    @number1 = @number2
+    @number2 = @number3
+
+    return @number3
+  end
 end
