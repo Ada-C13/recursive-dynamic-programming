ef16767eaef1f517d652ef813dafbd3a2f459357
Haben <haben7@gmail.com>
2020-05-04 21:21:06 -0700

    edited out comments

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -8,9 +8,9 @@
 def super_digit(n)
   return n if n < 10
 
-  chars_array = n.to_s.chars # ('9'  '8' '7'  '5')
-  int_array = chars_array.map(&:to_i) # [9 ,8, 7, 5]
-  sum = int_array.sum # 29
+  chars_array = n.to_s.chars 
+  int_array = chars_array.map(&:to_i) 
+  sum = int_array.sum 
 
   super_digit(sum)
 end
@@ -20,8 +20,7 @@ end
 # Space Complexity - O(1)
 def refined_super_digit(n, k)
  
-  return  super_digit((n.to_s * k).to_i) # ('148148148') => (148148148)
-    
+  return  super_digit((n.to_s * k).to_i) 
 end
 
     
