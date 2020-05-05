12c169d1c73941e512994cc33bf57a376c372208
Angela Nguyen <angethuy@gmail.com>
2020-05-04 17:49:33 -0700

    fibonacci and super_digit methods working

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,8 +1,8 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
-# Hint, you may want a recursive helper method
-def fibonacci(n)
- 
+# Time Complexity - O(n), n decrements by 1 each call 
+# Space Complexity - O(n), n frames added to stack
+def fibonacci(n, a=0, b=1)
+  raise ArgumentError, "input must be greater or equal to  0" if n < 0
+  return n == 0 ? a : fibonacci(n-1, b, a+b)
 end
