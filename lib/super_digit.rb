d8cd7b5f98985da995e2be88fb5e2199903640df
Sharon Cheung <keikei1128@g.ucla.edu>
2020-05-04 22:11:23 -0700

    finished assisgnment

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,34 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(log 10 n)
+# Space Complexity - O(log 10 n)
 def super_digit(n)
-   
+  if 10 > n
+    return n
+  end
+
+  last_digit = n % 10
+  sum = n / 10 + last_digit 
+  
+  return super_digit(sum)
 end
   
 
-# Time Complexity - ?
-# Space Complexity - ?
+# first attempt with build-in method
+# def super_digit(n)
+#   if 10 > n
+#     return n
+#   end
+
+#   return super_digit(n.digits.sum)
+# end
+  
+
+# Time Complexity - O(log 10 (n*k))
+# Space Complexity - O(log 10 (n*k))
+
+# not sure how to improve the complexity ....
+
 def refined_super_digit(n, k)
-    
+  super_digit(n*k)
 end
-    
