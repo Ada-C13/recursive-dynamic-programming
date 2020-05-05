fa83e14b9cfa1ddcca5974990d0a5635cdafef2e
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-05-04 20:04:12 -0700

    Added fibonacci and super_digit methods, and passed accompanying tests

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,9 +1,12 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n), where n is number of digits
+# Space Complexity - O(n), where n is number of digits, because of call stack
 def super_digit(n)
-   
+    # base case
+    return n if n < 10
+    # recursive case
+    return n%10 + super_digit(n/10) 
 end
   
 
