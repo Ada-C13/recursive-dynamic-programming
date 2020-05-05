f03a3980a5e6ae7d344ba58290aaefecca2a7c6f
Katie <katie.vandervoort@gmail.com>
2020-05-05 00:01:22 -0700

    implemented super digit and fibonacci

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,30 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(log10n) where n is num digits in n
+# Space Complexity - O(log10n) where n is num digits in n
 def super_digit(n)
-   
+  return n if n < 10
+
+   new_num = 0
+
+   n.to_s.length.times do |i|
+    new_num += n%10
+    n = n/10
+   end
+
+   return super_digit(new_num)
 end
   
 
+
+
+
+
+
+
 # Time Complexity - ?
 # Space Complexity - ?
-def refined_super_digit(n, k)
+# def refined_super_digit(n, k)
     
-end
+# end
     
