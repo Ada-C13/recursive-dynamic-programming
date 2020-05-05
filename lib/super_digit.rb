2ac9d7741f0d5e85b9c61b5d8057e51d2b762201
Stephanie <stephaniejmars@gmail.com>
2020-05-04 20:27:32 -0700

    refined added, super_digit cleaned up

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,13 +1,9 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n) where n is the amount of times the sum is greater than 10 
+# Space Complexity - O(n) where n is the number of digits in n
 def super_digit(n)
-  
-  if n < 10 
-    puts n
-    return n
-  end
+  return n if n < 10
 
   sum = 0 
 
@@ -22,12 +18,17 @@ def super_digit(n)
 end
   
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n)
+# Space Complexity - O(n)
 def refined_super_digit(n, k)
-    
-end
-    
+  refined = ""
 
-# super_digit(12345)
-# super_digit(1)
+  k.times do 
+    refined += n.to_s 
+  end 
+  refined = refined.to_i
+
+  super_digit(refined)
+
+end
+
