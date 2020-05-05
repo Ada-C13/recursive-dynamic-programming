79f93153b5fe163ba8c4d66a9ae884eb552fd75d
Stephanie <stephaniejmars@gmail.com>
2020-05-04 20:05:32 -0700

    super digit functional

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,7 +3,22 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-   
+  
+  if n < 10 
+    puts n
+    return n
+  end
+
+  sum = 0 
+
+  while n > 0
+    digit = n % 10
+    n /= 10
+    sum += digit
+  end
+
+  super_digit(sum)
+
 end
   
 
@@ -13,3 +28,6 @@ def refined_super_digit(n, k)
     
 end
     
+
+# super_digit(12345)
+# super_digit(1)
