6805ba779e1ddaa153b51910e7ad29be57ead92b
Lola Llanes <mslourdesllanes@gmail.com>
2020-05-05 23:59:19 -0700

    added super_digit and refined_super_digit method; passed tests

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,21 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity 0(log (n)) n is the number itself
+# Space Complexity 0(n)
 def super_digit(n)
-   
+  while n > 9
+    n = n % 10 + super_digit(n / 10)
+  end
+  return n
 end
   
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(log (n)) n is is the n being passed in
+# Space Complexity - O(log(n)) 
+# source https://www.codechef.com/problems/KC004
 def refined_super_digit(n, k)
-    
+  this_super_digit = super_digit(n)
+  
+  return super_digit(this_super_digit * k)
 end
     
