4d89b9d04c5baec995956df705e2f1568899521e
dnguye2 <dnguyen1093@gmail.com>
2020-05-05 18:52:52 -0700

    passes all superdigit tests

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,20 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n) - dependent on length of number
+# Space Complexity - O(n)
 def super_digit(n)
-   
+  n = n.digits
+  return n[0] if n.length == 1
+
+  n = n.inject(0) {|sum, i|  sum + i }
+
+  super_digit(n)
 end
   
 
 # Time Complexity - ?
 # Space Complexity - ?
-def refined_super_digit(n, k)
+# def refined_super_digit(n, k)
     
-end
+# end
     
